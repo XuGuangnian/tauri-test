bfd79609b0c364ca708dbbbad861e81419b5e58e
wanggengen <gengen.wang@outlook.com>
2025-07-29 17:52:28 +0800

    init commit

---
+++ src-tauri/gen/android/app/src/main/java/com/genge/tauri_test/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.genge.tauri_test
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
